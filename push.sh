a144fa3a04fc2b1ccfeb742bb8c69e799100cfbc
Carl <qwerfcxzas1@gmail.com>
2024-08-17 17:13:36 +0800

    update

--- push.sh
+++ push.sh
@@ -1,3 +1,8 @@
 git add .
 git commit -m "update"
-git push origin main
+output=$(git push origin main)
+if [[ $output == *"Everything up-to-date"* ]]; then
+    echo "已經是最新的版本，無需推送"
+else
+    echo "推送成功"
+fi
